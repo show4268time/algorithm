6042ab2d1a928cf9a4ad08b22cefc5efa7fc3587
show4268time <show4268time@163.com>
2022-05-05 12:25:31 +0800

    3. Longest Substring Without Repeating Characters

---
+++ src/main/java/leetcode/code3lengthOfLongestSubstring.java
@@ -0,0 +1,25 @@
+package main.java.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given a string s, find the length of the longest substring without repeating characters.
+ */
+public class code3lengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        int fastCursor = 0;
+        int slowCursor = 0;
+        Set<Character> set = new HashSet<>();
+        int max = 0;
+        while (fastCursor < s.length()) {
+            if (!set.contains(s.charAt(fastCursor))) {
+                set.add(s.charAt(fastCursor++));
+                max = Math.max(max,set.size());
+            } else {
+                set.remove(s.charAt(slowCursor++));
+            }
+        }
+        return max;
+    }
+}
