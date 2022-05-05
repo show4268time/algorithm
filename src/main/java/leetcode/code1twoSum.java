63150a39ebb01aee32f888af4464f9c77e0573ba
show4268time <show4268time@163.com>
2022-05-05 12:15:13 +0800

    1. Two Sum

---
+++ src/main/java/leetcode/code1twoSum.java
@@ -0,0 +1,21 @@
+package main.java.leetcode;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ */
+public class code1twoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 1; i < nums.length; i ++){
+            for(int j =i; j < nums.length; j++){
+                if(nums[j -i] + nums[j] == target){
+                    return new int[]{j -i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
