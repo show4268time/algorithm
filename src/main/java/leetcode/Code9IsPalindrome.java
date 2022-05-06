38eac3b9c84ecbe923411c40677f700f9444cc59
show4268time <show4268time@163.com>
2022-05-06 12:07:16 +0800

    9. Palindrome Number

---
+++ src/main/java/leetcode/Code9IsPalindrome.java
@@ -0,0 +1,27 @@
+package main.java.leetcode;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-05-06 12:04
+ * @Description: Given an integer x, return true if x is palindrome integer.
+ * An integer is a palindrome when it reads the same backward as forward.
+ * For example, 121 is a palindrome while 123 is not.
+ */
+public class Code9IsPalindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        String str = String.valueOf(x);
+        int start = 0;
+        int end = str.length() - 1;
+        while (start <= end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
