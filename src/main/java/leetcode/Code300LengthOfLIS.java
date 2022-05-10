f6b24d8157a202573c8bc696afc3e9ec78ec89ec
show4268time <show4268time@163.com>
2022-05-10 22:35:28 +0800

    322. Coin Change

---
+++ src/main/java/leetcode/Code300LengthOfLIS.java
@@ -0,0 +1,22 @@
+package main.java.leetcode;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-05-10 18:09
+ * @Description: Given an integer array nums, return the length of the longest strictly increasing subsequence.
+ * A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements.
+ * For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+ */
+public class Code300LengthOfLIS {
+    public int lengthOfLIS(int[] nums) {
+        Integer max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            int left, right = i;
+            while (left>= 0 && right)
+        }
+    }
+
+    private int dp(int i, int j) {
+
+    }
+}
