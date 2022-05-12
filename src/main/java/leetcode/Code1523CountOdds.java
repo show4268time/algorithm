910b09bf17027d7f755020ee2844a60a4410dd13
show4268time <show4268time@163.com>
2022-05-12 17:49:11 +0800

    144. Binary Tree Preorder Traversal

---
+++ src/main/java/leetcode/Code1523CountOdds.java
@@ -0,0 +1,13 @@
+package main.java.leetcode;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-05-10 23:23
+ * @Description:
+ * Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+ */
+public class Code1523CountOdds {
+    public int countOdds(int low, int high) {
+
+    }
+}
