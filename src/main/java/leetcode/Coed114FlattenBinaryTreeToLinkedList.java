e2a2055d39b641de02d0d14ef995a4f2c6d64e06
show4268time <show4268time@163.com>
2022-05-14 10:34:19 +0800

    114. Flatten Binary Tree to Linked List

---
+++ src/main/java/leetcode/Coed114FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-05-13 23:34
+ * @Description: Given the root of a binary tree, flatten the tree into a "linked list":
+ * The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+ * The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ */
+public class Coed114FlattenBinaryTreeToLinkedList {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public void flatten(TreeNode root) {
+        // base case
+        if (root == null) return;
+
+        // 利用定义，把左右子树拉平
+        flatten(root.left);
+        flatten(root.right);
+
+        /**** 后序遍历位置 ****/
+        // 1、左右子树已经被拉平成一条链表
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        // 2、将左子树作为右子树
+        root.left = null;
+        root.right = left;
+
+        // 3、将原先的右子树接到当前右子树的末端
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = right;
+    }
+}
