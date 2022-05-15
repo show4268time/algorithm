d17ba5f63ca8465f3cd9013c0340c3612ec0a24d
show4268time <show4268time@163.com>
2022-05-15 09:32:20 +0800

    19. Remove Nth Node From End of List

---
+++ src/main/java/leetcode/Code19RemoveNthFromEnd.java
@@ -0,0 +1,54 @@
+package main.java.leetcode;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-05-10 09:47
+ * @Description: Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ */
+public class Code19RemoveNthFromEnd {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) return null;
+        ListNode sentinel = new ListNode();
+        sentinel.next = head;
+
+        ListNode listNode = findFromEnd(sentinel, n + 1);
+        if (listNode != null) {
+            listNode.next = listNode.next.next;
+        }
+
+        return sentinel.next;
+    }
+
+    ListNode findFromEnd(ListNode head, int k) {
+        ListNode p1 = head;
+        // p1 先走 k 步
+        for (int i = 0; i < k; i++) {
+            p1 = p1.next;
+        }
+        ListNode p2 = head;
+        // p1 和 p2 同时走 n - k 步
+        while (p1 != null) {
+            p2 = p2.next;
+            p1 = p1.next;
+        }
+        // p2 现在指向第 n - k 个节点
+        return p2;
+    }
+}
