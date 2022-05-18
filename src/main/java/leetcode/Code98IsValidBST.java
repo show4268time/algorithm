4daa739648c6b0d12aa6ef7f43061066d7078348
show4268time <show4268time@163.com>
2022-05-18 10:15:58 +0800

    98. Validate Binary Search Tree

---
+++ src/main/java/leetcode/Code98IsValidBST.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-05-18 10:14
+ * @Description: Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+ * A valid BST is defined as follows:
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ */
+public class Code98IsValidBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+
+
+        return isValidBST(root, null, null);
+
+    }
+
+    /* 限定以 root 为根的子树节点必须满足 max.val > root.val > min.val */
+    boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+        // base case
+        if (root == null) return true;
+        // 若 root.val 不符合 max 和 min 的限制，说明不是合法 BST
+        if (min != null && root.val <= min.val) return false;
+        if (max != null && root.val >= max.val) return false;
+        // 限定左子树的最大值是 root.val，右子树的最小值是 root.val
+        return isValidBST(root.left, min, root)
+                && isValidBST(root.right, root, max);
+    }
+}
