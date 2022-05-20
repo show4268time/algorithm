e9a9144d052747ea92228e6a243096d3c7810921
show4268time <show4268time@163.com>
2022-05-20 08:35:08 +0800

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- src/main/java/leetcode/Code111MinDepth.java
+++ src/main/java/leetcode/Code111MinDepth.java
@@ -20,18 +20,6 @@ public class Code111MinDepth {
         }
     }
 
-    public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(20);
-        node1.left =  new TreeNode(15);
-        node1.right =  new TreeNode(17);
-
-        TreeNode root = new TreeNode(30);
-        root.left = new TreeNode(9);
-        root.right = node1;
-
-        BFS(root);
-    }
-
     public int minDepth(TreeNode root) {
         if (root == null) return 0;
         if (root.left == null && root.right == null) return 1;
