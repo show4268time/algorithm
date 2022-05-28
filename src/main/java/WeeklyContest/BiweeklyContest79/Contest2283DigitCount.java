34feddf43c7c8212ada75a4f6182482d2732c2da
show4268time <show4268time@163.com>
2022-05-29 07:51:29 +0800

    2283. Check if Number Has Equal Digit Count and Digit Value

---
+++ src/main/java/WeeklyContest/BiweeklyContest79/Contest2283DigitCount.java
@@ -0,0 +1,29 @@
+package WeeklyContest.BiweeklyContest79;
+
+import java.util.Arrays;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-05-29 07:50
+ * @Description: You are given a 0-indexed string num of length n consisting of digits.
+ * Return true if for every index i in the range 0 <= i < n, the digit i occurs num[i] times in num, otherwise return false.
+ */
+public class Contest2283DigitCount {
+    public boolean digitCount(String num) {
+        int[] count = new int[num.length()];
+        Arrays.fill(count, 0);
+        char[] chars = num.toCharArray();
+        for (char c : chars) {
+            int i = c - '0';
+            if (i < chars.length) {
+                count[c - '0']++;
+            }
+        }
+        for (int i = 0; i < num.length(); i++) {
+            if (chars[i] - '0' != count[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
