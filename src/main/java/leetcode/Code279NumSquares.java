ac6f58bdd7d8d0e7ef094c514d0be1673b94bf81
show4268time <show4268time@163.com>
2022-06-11 13:00:41 +0800

    279. Perfect Squares

--- src/main/java/leetcode/Code279NumSquares.java
+++ src/main/java/leetcode/Code279NumSquares.java
@@ -28,8 +28,8 @@ public class Code279NumSquares {
 
         dp[0][0] = 0;
 
-        for (int i = 1; i <= length; i++) {
-            int num = list.get(i - 1);
+        for (int i = 1; i *i <= length; i++) {
+            int num = i *i;
             for (int j = 0; j <= n; j++) {
                 dp[i][j] = dp[i - 1][j];
                 if (num <= j) {
@@ -37,7 +37,6 @@ public class Code279NumSquares {
                 }
             }
         }
-
         return dp[length][n];
     }
 }
