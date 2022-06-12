4bab957863e75f76fc0364546804c34172de6af8
show4268time <show4268time@163.com>
2022-06-12 14:16:40 +0800

    2304. Minimum Path Cost in a Grid

--- src/main/java/WeeklyContest/WeeklyContest297/Contest2303CalculateTax.java
+++ src/main/java/WeeklyContest/WeeklyContest297/Contest2303CalculateTax.java
@@ -4,9 +4,9 @@ package WeeklyContest.WeeklyContest297;
  * @author: PhilipFry
  * @create: 2022-06-12 12:43
  * @Description:You are given a 0- indexed (指数) 2D integer array brackets where brackets[i] = [upperi, percenti] means that the ith tax bracket (支架) has an upper bound of upperi and is taxed at a rate of percenti. The brackets (支架) are sorted by upper bound (i.e. upperi-1 < upperi for 0 < i < brackets.length).
- *
+ * <p>
  * Tax is calculated (计算) as follows:
- *
+ * <p>
  * The first upper0 dollars earned are taxed at a rate of percent0.
  * The next upper1 - upper0 dollars earned are taxed at a rate of percent1.
  * The next upper2 - upper1 dollars earned are taxed at a rate of percent2.
