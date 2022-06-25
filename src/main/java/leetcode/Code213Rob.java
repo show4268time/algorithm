b80dd398ab867cc3e9f3d8f8ba1e188b22cb0515
show4268time <show4268time@163.com>
2022-06-25 17:39:46 +0800

    213. House Robber II

---
+++ src/main/java/leetcode/Code213Rob.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-06-25 17:38
+ * @Description: You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed.
+ * <p>
+ * All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one.
+ * <p>
+ * Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+ * <p>
+ * Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+ */
+public class Code213Rob {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        int[] f = new int[n + 1];
+        int[] g = new int[n + 1];
+        int res = 0;
+        for (int i = 1; i < n; i++) {
+            f[i] = g[i - 1] + nums[i - 1];
+            g[i] = Math.max(f[i - 1], g[i - 1]);
+        }
+        res = Math.max(f[n - 1], g[n - 1]);
+
+        Arrays.fill(f, 0);
+        Arrays.fill(g, 0);
+        for (int i = 2; i <= n; i++) {
+            f[i] = g[i - 1] + nums[i - 1];
+            g[i] = Math.max(f[i - 1], g[i - 1]);
+        }
+        res = Math.max(res, f[n]);
+        res = Math.max(res, g[n]);
+        return res;
+    }
+}
