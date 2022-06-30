b958887dc8fbda8d677c0e6f83a11a929eed3637
show4268time <show4268time@163.com>
2022-06-30 23:44:30 +0800

    7. Reverse Integer

--- src/main/java/leetcode/code50/Code7ReverseInteger.java
+++ src/main/java/leetcode/code50/Code7ReverseInteger.java
@@ -3,10 +3,17 @@ package leetcode.code50;
 /**
  * @author: PhilipFry
  * @create: 2022-05-06 10:38
- * @Description:
- * Given a signed 32-bit integer x, return x with its digits reversed.
+ * @Description: Given a signed 32-bit integer x, return x with its digits reversed.
  * If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
  * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
  */
 public class Code7ReverseInteger {
+    public int reverse(int x) {
+        long res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+        return res > Integer.MAX_VALUE || res < Integer.MIN_VALUE ? 0 : (int) res;
+    }
 }
