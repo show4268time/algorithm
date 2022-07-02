51185f6ccafa0ffa0ae7d78afc0bffa461292f69
show4268time <show4268time@163.com>
2022-07-02 13:53:50 +0800

    21. 合并两个有序链表

--- src/main/java/leetcode/code50/Code21MergeTwoLists.java
+++ src/main/java/leetcode/code50/Code21MergeTwoLists.java
@@ -31,6 +31,38 @@ public class Code21MergeTwoLists {
         return cur.next;
     }
 
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        if(list1 == null && list2 == null){
+            return null;
+        }
+
+        if(list1 == null || list2 == null){
+            return list1 == null ? list2 : list1;
+        }
+
+        ListNode sentinel = new ListNode();
+        ListNode cur = sentinel;
+        while(list1 != null || list2 != null){
+            if(list1 == null){
+                cur.next = list2;
+                break;
+            }else if(list2 == null){
+                cur.next = list1;
+                break;
+            }else{
+                if(list1.val <= list2.val){
+                    cur.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                }else{
+                    cur.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                }
+            }
+            cur = cur.next;
+        }
+        return sentinel.next;
+    }
+
     public void mergeTwoLists(ListNode list1, ListNode list2, ListNode cur) {
         if (list1 == null) {
             cur.next = list2;
