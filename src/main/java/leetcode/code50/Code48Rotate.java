3f5a475aba71a8539ea5816df6bf840cac00c987
show4268time <show4268time@163.com>
2022-07-07 22:40:45 +0800

    48. 旋转图像

---
+++ src/main/java/leetcode/code50/Code48Rotate.java
@@ -0,0 +1,30 @@
+package leetcode.code50;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-07-07 22:39
+ * @Description: 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ * <p>
+ * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ */
+public class Code48Rotate {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0, k = n - 1; j < k; j++, k--) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = temp;
+            }
+        }
+    }
+}
