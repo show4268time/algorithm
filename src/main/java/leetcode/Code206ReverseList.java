385c8e47bc324ebed375f251a52f1e8b2c559e92
show4268time <show4268time@163.com>
2022-07-23 18:01:18 +0800

    206. 反转链表

---
+++ src/main/java/leetcode/Code206ReverseList.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-07-23 17:48
+ * @Description: 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ */
+public class Code206ReverseList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode tail = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return tail;
+    }
+    public ListNode reverseList1(ListNode head) {
+        if(head == null) return null;
+
+        ListNode l = head, r = head.next;
+        while (r != null){
+            ListNode n = r.next;
+            r.next = l;
+            l.next = null;
+            l = r;
+            r = n;
+        }
+
+        return l;
+    }
+}
