8857e144f8ff33b6421c7cd48ef4343374e6e99b
show4268time <show4268time@163.com>
2022-07-25 23:40:19 +0800

    337. 打家劫舍 III

---
+++ src/main/java/leetcode/Code337Rob.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-07-25 23:39
+ * @Description: 小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为 root 。
+ * <p>
+ * 除了 root 之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。
+ * <p>
+ * 如果 两个直接相连的房子在同一天晚上被打劫 ，房屋将自动报警。
+ * <p>
+ * 给定二叉树的 root 。返回 在不触动警报的情况下 ，小偷能够盗取的最高金额 。
+ */
+public class Code337Rob {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int rob(TreeNode root) {
+        int[] res = dfs(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    private int[] dfs(TreeNode node) {
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+
+        int[] l = dfs(node.left);
+        int[] r = dfs(node.right);
+
+        return new int[]{Math.max(l[0], l[1]) + Math.max(r[0], r[1]), node.val + l[0] + r[0]};
+    }
+}
