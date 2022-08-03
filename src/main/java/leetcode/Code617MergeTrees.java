08fee082cf399438648b9ccdd8de2c39d8d578e1
show4268time <show4268time@163.com>
2022-08-03 23:46:29 +0800

    617. 合并二叉树

---
+++ src/main/java/leetcode/Code617MergeTrees.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-08-03 23:45
+ * @Description: 给你两棵二叉树： root1 和 root2 。
+ * <p>
+ * 想象一下，当你将其中一棵覆盖到另一棵之上时，两棵树上的一些节点将会重叠（而另一些不会）。你需要将这两棵树合并成一棵新二叉树。合并的规则是：如果两个节点重叠，那么将这两个节点的值相加作为合并后节点的新值；否则，不为 null 的节点将直接作为新二叉树的节点。
+ * <p>
+ * 返回合并后的二叉树。
+ * <p>
+ * 注意: 合并过程必须从两个树的根节点开始。
+ */
+public class Code617MergeTrees {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) return t2;
+
+        if (t2 == null) return t1;
+
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+
+        return t1;
+    }
+}
