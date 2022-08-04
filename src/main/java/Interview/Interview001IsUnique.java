4201897774b10fb712686606a801277ef678fb63
show4268time <show4268time@163.com>
2022-08-04 23:28:32 +0800

    面试题 01.01. 判定字符是否唯一

---
+++ src/main/java/Interview/Interview001IsUnique.java
@@ -0,0 +1,33 @@
+package Interview;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-08-04 23:27
+ * @Description: 实现一个算法，确定一个字符串 s 的所有字符是否全都不同。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: s = "leetcode"
+ * 输出: false
+ * 示例 2：
+ * <p>
+ * 输入: s = "abc"
+ * 输出: true
+ * 限制：
+ * <p>
+ * 0 <= len(s) <= 100
+ * s[i]仅包含小写字母
+ * 如果你不使用额外的数据结构，会很加分。
+ */
+public class Interview001IsUnique {
+    public boolean isUnique(String astr) {
+        int n = astr.length();
+        int[] array = new int[256];
+        char[] chars = astr.toCharArray();
+        for (int i = 0; i < n; i++) {
+            if (array[chars[i]] == 1) return false;
+            array[chars[i]]++;
+        }
+        return true;
+    }
+}
