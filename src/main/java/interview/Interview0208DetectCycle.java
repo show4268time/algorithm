4c5bfb3c17918770db304a445c57d2ce2b4a1934
show4268time <show4268time@163.com>
2022-08-05 23:40:47 +0800

    面试题 02.08. 环路检测

---
+++ src/main/java/interview/Interview0208DetectCycle.java
@@ -0,0 +1,42 @@
+package interview;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-08-05 23:39
+ * @Description: 给定一个链表，如果它是有环链表，实现一个算法返回环路的开头节点。若环不存在，请返回 null。
+ * <p>
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ */
+public class Interview0208DetectCycle {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                return null;
+            }
+
+            if (fast == slow) {
+                ListNode node = head;
+                while (node != slow) {
+                    node = node.next;
+                    slow = slow.next;
+                }
+                return node;
+            }
+        }
+        return null;
+    }
+}
