c901717216e7461e5748c62a5f97cd409432d3fe
show4268time <show4268time@163.com>
2022-08-05 23:39:09 +0800

    面试题 02.07. 链表相交

---
+++ src/main/java/interview/Interview0207GetIntersectionNode.java
@@ -0,0 +1,41 @@
+package interview;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-08-05 23:37
+ * @Description: 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+ * <p>
+ * 图示两个链表在节点 c1 开始相交：
+ * <p>
+ * <p>
+ * <p>
+ * 题目数据 保证 整个链式结构中不存在环。
+ * <p>
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ */
+public class Interview0207GetIntersectionNode {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode node1 = headA, node2 = headB;
+        while (node1 != node2) {
+
+            node1 = node1 == null ? headB : node1.next;
+            node2 = node2 == null ? headA : node2.next;
+        }
+
+        return node1;
+    }
+}
