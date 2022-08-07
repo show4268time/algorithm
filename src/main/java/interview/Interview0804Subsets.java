be2dd34e167870c23d7160b73719fa6fcdd6b2a3
show4268time <show4268time@163.com>
2022-08-07 17:57:08 +0800

    面试题 08.04. 幂集

--- src/main/java/interview/Interview0804Subsets.java
+++ src/main/java/interview/Interview0804Subsets.java
@@ -7,7 +7,21 @@ import java.util.List;
 /**
  * @author: PhilipFry
  * @create: 2022-08-07 17:55
- * @Description:
+ * @Description: 幂集。编写一种方法，返回某集合的所有子集。集合中不包含重复的元素。
+ * <p>
+ * 说明：解集不能包含重复的子集。
+ * 输入： nums = [1,2,3]
+ * 输出：
+ * [
+ * [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
  */
 public class Interview0804Subsets {
     List<List<Integer>> res = new ArrayList<>();
