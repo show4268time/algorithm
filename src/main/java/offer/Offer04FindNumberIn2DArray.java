ec4e0653bcdc90217e3b1dc17cd625b637ef6e1c
show4268time <show4268time@163.com>
2022-08-10 23:52:17 +0800

    剑指 Offer 04. 二维数组中的查找

---
+++ src/main/java/offer/Offer04FindNumberIn2DArray.java
@@ -0,0 +1,26 @@
+package offer;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-08-10 23:03
+ * @Description:
+ */
+public class Offer04FindNumberIn2DArray {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) return true;
+
+            if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
