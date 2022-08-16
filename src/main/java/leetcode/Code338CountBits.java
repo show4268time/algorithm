433cdf2da9802a508487e2180948e6c9e16e71ad
show4268time <show4268time@163.com>
2022-08-16 23:26:31 +0800

    338. Counting Bits

--- src/main/java/leetcode/Code338CountBits.java
+++ src/main/java/leetcode/Code338CountBits.java
@@ -4,7 +4,28 @@ package leetcode;
  * @author: PhilipFry
  * @create: 2022-06-25 19:44
  * @Description: Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n),
+ * <p>
  * ans[i] is the number of 1's in the binary representation of i.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 2
+ * Output: [0,1,1]
+ * Explanation:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * Example 2:
+ * <p>
+ * Input: n = 5
+ * Output: [0,1,1,2,1,2]
+ * Explanation:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * 3 --> 11
+ * 4 --> 100
+ * 5 --> 101
  */
 public class Code338CountBits {
     public int[] countBits(int n) {
