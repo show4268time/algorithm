c6480f311968e83f528331bef6300f70c8c25197
show4268time <show4268time@163.com>
2022-08-18 23:25:33 +0800

    560. Subarray Sum Equals K

--- src/main/java/leetcode/Code560SubarraySum.java
+++ src/main/java/leetcode/Code560SubarraySum.java
@@ -9,17 +9,27 @@ import java.util.Map;
  * @Description: Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
  * <p>
  * A subarray is a contiguous non-empty sequence of elements within an array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,1,1], k = 2
+ * Output: 2
+ * Example 2:
+ * <p>
+ * Input: nums Ø= [1,2,3], k = 3
+ * Output: 2
  */
 public class Code560SubarraySum {
     public int subarraySum(int[] nums, int k) {
         int res = 0;
-        int n = 0;
         Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
         map.put(0, 1);
         for (int i = 0; i < nums.length; i++) {
-            n += nums[i];
-            res += map.getOrDefault(n - k, 0);
-            map.put(n, map.getOrDefault(n, 0) + 1);
+            sum += nums[i];
+            int t = sum - k;
+            res += map.getOrDefault(t, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return res;
     }
