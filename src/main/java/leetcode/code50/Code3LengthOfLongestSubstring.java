482209f85c27e06f78cdfc84815b56a30010dc8e
show4268time <show4268time@163.com>
2022-08-19 23:35:04 +0800

    3. Longest Substring Without Repeating Characters

--- src/main/java/leetcode/code50/Code3LengthOfLongestSubstring.java
+++ src/main/java/leetcode/code50/Code3LengthOfLongestSubstring.java
@@ -2,18 +2,37 @@ package leetcode.code50;
 
 /**
  * Given a string s, find the length of the longest substring without repeating characters.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ * Example 2:
+ * <p>
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * Example 3:
+ * <p>
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 public class Code3LengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s) {
-        int[] array = new int[128];
         int res = 0;
-        for (int left = 0, right = 0; right < s.length(); right++) {
-            array[s.charAt(right)]++;
-            while (array[s.charAt(right)] > 1) {
-                array[s.charAt(left)]--;
-                left++;
+        char[] chars = s.toCharArray();
+        int[] cur = new int[128];
+        for (int i = 0, j = 0; i < chars.length; i++) {
+            while (cur[chars[i]] == 1) {
+                cur[chars[j++]]--;
             }
-            res = Math.max(res, right - left + 1);
+            cur[chars[i]]++;
+            res = Math.max(i - j + 1, res);
         }
         return res;
     }
