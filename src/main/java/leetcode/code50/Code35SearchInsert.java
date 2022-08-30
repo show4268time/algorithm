1290a7c16410c121f43063acd80c21d01a58e869
show4268time <show4268time@163.com>
2022-08-30 23:17:15 +0800

    35. 搜索插入位置

---
+++ src/main/java/leetcode/code50/Code35SearchInsert.java
@@ -0,0 +1,30 @@
+package leetcode.code50;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-08-30 23:16
+ * @Description: 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+ * <p>
+ * 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * <p>
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ * <p>
+ *    示例 1:  输入: nums = [1,3,5,6], target = 5 输出: 2
+ * <p>
+ * 示例 2:  输入: nums = [1,3,5,6], target = 2 输出: 1
+ * <p>
+ * 示例 3:  输入: nums = [1,3,5,6], target = 7 输出: 4  
+ */
+public class Code35SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (nums[mid] <= target) l = mid;
+            else r = mid - 1;
+        }
+        if (nums[l] == target) return l;
+        else if (l == 0 && nums[l] > target) return 0;
+        else return l + 1;
+    }
+}
