15b66644f2df4cb2618257a05df2ad6e5600098b
show4268time <show4268time@163.com>
2022-08-30 23:57:36 +0800

    852. Peak Index in a Mountain Array

---
+++ src/main/java/leetcode/Code852PeakIndexInMountainArray.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-08-30 23:56
+ * @Description: An array arr a mountain if the following properties hold:
+ * <p>
+ * arr.length >= 3
+ * There exists some i with 0 < i < arr.length - 1 such that:
+ * arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+ * arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+ * Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+ * <p>
+ * You must solve it in O(log(arr.length)) time complexity.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: arr = [0,1,0]
+ * Output: 1
+ * Example 2:
+ * <p>
+ * Input: arr = [0,2,1,0]
+ * Output: 1
+ * Example 3:
+ * <p>
+ * Input: arr = [0,10,5,2]
+ * Output: 1
+ */
+public class Code852PeakIndexInMountainArray {
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 1, r = arr.length - 2;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (arr[mid] > arr[mid - 1]) l = mid;
+            else r = mid - 1;
+        }
+        return l;
+    }
+}
