3dad3b37eaa7b1a991238fd88ce7e766466d6426
show4268time <show4268time@163.com>
2022-08-31 23:43:12 +0800

    78. Subsets

--- src/main/java/leetcode/code100/Code78Subsets.java
+++ src/main/java/leetcode/code100/Code78Subsets.java
@@ -8,7 +8,17 @@ import java.util.List;
  * @author: PhilipFry
  * @create: 2022-06-04 08:52
  * @Description: Given an integer array nums of unique elements, return all possible subsets (the power set).
+ * <p>
  * The solution set must not contain duplicate subsets. Return the solution in any order.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3]
+ * Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ * Example 2:
+ * <p>
+ * Input: nums = [0]
+ * Output: [[],[0]]
  */
 public class Code78Subsets {
     List<List<Integer>> res = new ArrayList<>();
