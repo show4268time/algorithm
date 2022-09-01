e2c3075140895db9b6df7ca95954627938967042
show4268time <show4268time@163.com>
2022-09-01 21:55:27 +0800

    40. Combination Sum II

--- src/main/java/leetcode/code50/Code40CombinationSum2.java
+++ src/main/java/leetcode/code50/Code40CombinationSum2.java
@@ -9,9 +9,31 @@ import java.util.List;
  * @author: PhilipFry
  * @create: 2022-06-03 23:49
  * @Description: Given a collection of candidate numbers (candidates) and a target number (target),
+ * <p>
  * find all unique combinations in candidates where the candidate numbers sum to target.
+ * <p>
  * Each number in candidates may only be used once in the combination.
+ * <p>
  * Note: The solution set must not contain duplicate combinations.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: candidates = [10,1,2,7,6,1,5], target = 8
+ * Output:
+ * [
+ * [1,1,6],
+ * [1,2,5],
+ * [1,7],
+ * [2,6]
+ * ]
+ * Example 2:
+ * <p>
+ * Input: candidates = [2,5,2,1,2], target = 5
+ * Output:
+ * [
+ * [1,2,2],
+ * [5]
+ * ]
  */
 public class Code40CombinationSum2 {
     List<List<Integer>> res = new ArrayList<>();
