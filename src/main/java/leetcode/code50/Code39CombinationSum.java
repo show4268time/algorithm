ffb52140adfacc01ea3193113bef776848ed9bea
show4268time <show4268time@163.com>
2022-09-01 21:48:07 +0800

    39. Combination Sum

--- src/main/java/leetcode/code50/Code39CombinationSum.java
+++ src/main/java/leetcode/code50/Code39CombinationSum.java
@@ -7,12 +7,34 @@ import java.util.List;
 /**
  * @author: PhilipFry
  * @create: 2022-06-03 23:04
- * @Description: Given an array (阵列) of distinct (鲜明) integers (整数) candidates and a target integer (整数) target,
- * return a list of all unique combinations of candidates where the chosen numbers sum (总和) to target.
+ * @Description: Given an array of distinct integers candidates and a target integer target,
+ * <p>
+ * return a list of all unique combinations of candidates where the chosen numbers sum to target.
+ * <p>
  * You may return the combinations in any order.
- * The same number may be chosen from candidates an unlimited (无限) number of times.
- * Two combinations are unique if the frequency (频率) of at least one of the chosen numbers is different.
- * It is guaranteed (保证) that the number of unique combinations that sum (总和) up to target is less than 150 combinations for the given input.
+ * <p>
+ * The same number may be chosen from candidates an unlimited number of times.
+ * <p>
+ * Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+ * <p>
+ * It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: candidates = [2,3,6,7], target = 7
+ * Output: [[2,2,3],[7]]
+ * Explanation:
+ * 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+ * 7 is a candidate, and 7 = 7.
+ * These are the only two combinations.
+ * Example 2:
+ * <p>
+ * Input: candidates = [2,3,5], target = 8
+ * Output: [[2,2,2,2],[2,3,3],[3,5]]
+ * Example 3:
+ * <p>
+ * Input: candidates = [2], target = 1
+ * Output: []
  */
 public class Code39CombinationSum {
     List<List<Integer>> result = new ArrayList<>();
