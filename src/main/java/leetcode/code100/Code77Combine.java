cb9d78daf4e6be95313a9c928cc8ccc6ab4384c3
show4268time <show4268time@163.com>
2022-09-01 09:23:06 +0800

    77. Combinations

--- src/main/java/leetcode/code100/Code77Combine.java
+++ src/main/java/leetcode/code100/Code77Combine.java
@@ -7,8 +7,23 @@ import java.util.List;
 /**
  * @author: PhilipFry
  * @create: 2022-06-04 16:22
- * @Description: Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+ * @Description: Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+ * <p>
  * You may return the answer in any order.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 4, k = 2
+ * Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+ * Explanation: There are 4 choose 2 = 6 total combinations.
+ * Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+ * Example 2:
+ * <p>
+ * Input: n = 1, k = 1
+ * Output: [[1]]
+ * Explanation: There is 1 choose 1 = 1 total combination.
  */
 public class Code77Combine {
     List<List<Integer>> result = new ArrayList<>();
