45f76d84184162c9367a67856484c558313b0114
show4268time <show4268time@163.com>
2022-09-02 20:51:00 +0800

    198. House Robber

--- src/main/java/leetcode/code200/Code198Rob.java
+++ src/main/java/leetcode/code200/Code198Rob.java
@@ -3,9 +3,30 @@ package leetcode.code200;
 /**
  * @author: PhilipFry
  * @create: 2022-06-25 17:36
- * @Description: 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * @Description: You are a professional robber planning to rob houses along a street.
  * <p>
- * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ * Each house has a certain amount of money stashed,
+ * <p>
+ * the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected
+ * <p>
+ * and it will automatically contact the police if two adjacent houses were broken into on the same night.
+ * <p>
+ * Given an integer array nums representing the amount of money of each house,
+ * <p>
+ * return the maximum amount of money you can rob tonight without alerting the police.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3,1]
+ * Output:
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+ * Total amount you can rob = 1 + 3 = 4.
+ * Example 2:
+ * <p>
+ * Input: nums = [2,7,9,3,1]
+ * Output: 12
+ * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+ * Total amount you can rob = 2 + 9 + 1 = 12.
  */
 public class Code198Rob {
     public int rob(int[] nums) {
