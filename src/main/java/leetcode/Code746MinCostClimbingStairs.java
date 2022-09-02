4078702bb18903307957c2cc3573fa8f3b4ce529
show4268time <show4268time@163.com>
2022-09-02 20:42:50 +0800

    746. Min Cost Climbing Stairs

---
+++ src/main/java/leetcode/Code746MinCostClimbingStairs.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-09-02 20:42
+ * @Description: You are given an integer array cost where cost[i] is the cost of ith step on a staircase.
+ * <p>
+ * Once you pay the cost, you can either climb one or two steps.
+ * <p>
+ * You can either start from the step with index 0, or the step with index 1.
+ * <p>
+ * Return the minimum cost to reach the top of the floor.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: cost = [10,15,20]
+ * Output: 15
+ * Explanation: You will start at index 1.
+ * - Pay 15 and climb two steps to reach the top.
+ * The total cost is 15.
+ * Example 2:
+ * <p>
+ * Input: cost = [1,100,1,1,1,100,1,1,100,1]
+ * Output: 6
+ * Explanation: You will start at index 0.
+ * - Pay 1 and climb two steps to reach index 2.
+ * - Pay 1 and climb two steps to reach index 4.
+ * - Pay 1 and climb two steps to reach index 6.
+ * - Pay 1 and climb one step to reach index 7.
+ * - Pay 1 and climb two steps to reach index 9.
+ * - Pay 1 and climb one step to reach the top.
+ * The total cost is 6.
+ */
+public class Code746MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min((dp[i - 2] + cost[i - 2]), (dp[i - 1] + cost[i - 1]));
+        }
+        return dp[n];
+    }
+}
