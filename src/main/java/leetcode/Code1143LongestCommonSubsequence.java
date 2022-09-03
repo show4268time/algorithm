a345527c0ec4d5ccf03b6e6dfdc41790038b5940
show4268time <show4268time@163.com>
2022-09-03 10:55:41 +0800

    1143. Longest Common Subsequence

--- src/main/java/leetcode/Code1143LongestCommonSubsequence.java
+++ src/main/java/leetcode/Code1143LongestCommonSubsequence.java
@@ -12,19 +12,17 @@ package leetcode;
  */
 public class Code1143LongestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
-        int n1 = text1.length();
-        int n2 = text2.length();
-
-        int[][] dp = new int[n1 + 1][n2 + 1];
-
-        for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
                 dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
                 }
             }
         }
-        return dp[n1][n2];
+        return dp[n][m];
     }
 }
