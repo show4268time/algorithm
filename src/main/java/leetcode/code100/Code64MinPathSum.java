17adfdc14277e85c0585dd1e102171f7835e2eac
show4268time <show4268time@163.com>
2022-09-03 12:47:54 +0800

    64. Minimum Path Sum

--- src/main/java/leetcode/code100/Code64MinPathSum.java
+++ src/main/java/leetcode/code100/Code64MinPathSum.java
@@ -1,5 +1,7 @@
 package leetcode.code100;
 
+import java.util.Arrays;
+
 /**
  * @author: PhilipFry
  * @create: 2022-07-09 14:34
@@ -26,5 +28,21 @@ public class Code64MinPathSum {
             }
         }
         return dp[m - 1][n - 1];
+    }
+
+    public int minPathSum1(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dp = new int[n + 1][m + 1];
+        dp[1][1] = grid[0][0];
+        Arrays.fill(dp[0], Integer.MAX_VALUE);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 && j == 1) continue;
+                if (j == 1) dp[i][j - 1] = Integer.MAX_VALUE;
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+            }
+        }
+        return dp[n][m];
     }
 }
