6b02a9aec7dd0168d510086923f7e4516f8158c1
show4268time <show4268time@163.com>
2022-09-03 12:39:33 +0800

    62. Unique Paths

--- src/main/java/leetcode/code100/Code62UniquePaths.java
+++ src/main/java/leetcode/code100/Code62UniquePaths.java
@@ -3,25 +3,28 @@ package leetcode.code100;
 /**
  * @author: PhilipFry
  * @create: 2022-07-09 14:23
- * @Description: 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * @Description: There is a robot on an m x n grid.
  * <p>
- * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * The robot is initially located at the top-left corner (i.e., grid[0][0]).
  * <p>
- * 问总共有多少条不同的路径？
+ * The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
+ * <p>
+ * The robot can only move either down or right at any point in time.
+ * <p>
+ * Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+ * <p>
+ * The test cases are generated so that the answer will be less than or equal to 2 * 109.
  */
 public class Code62UniquePaths {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j  < n; j++){
-                if(i == 0 || j==0){
-                    dp[i][j] = 1;
-                } else{
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
+        int[][] dp = new int[m + 1][n + 1];
+        dp[1][1] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 && j == 1) continue;
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        return dp[m-1][n-1];
+        return dp[m][n];
     }
 }
