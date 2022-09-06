01e5cc5cd930bf76677cf9e511763c3181025aa4
show4268time <show4268time@163.com>
2022-09-06 23:27:11 +0800

    72. Edit Distance

---
+++ src/main/java/leetcode/code100/Code72MinDistance.java
@@ -0,0 +1,35 @@
+package leetcode.code100;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-07-09 15:23
+ * @Description: 给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数  。
+ * <p>
+ * 你可以对一个单词进行如下三种操作：
+ * <p>
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ */
+public class Code72MinDistance {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+        dp[0][0] = 0;
+        for (int i = 1; i <= m; i++) dp[i][0] = i;
+        for (int j = 1; j <= n; j++) dp[0][j] = j;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char c1 = word1.charAt(i - 1);
+                char c2 = word2.charAt(j - 1);
+                dp[i][j] = dp[i - 1][j - 1] + (c1 == c2 ? 0 : 1);
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+
+            }
+        }
+        return dp[m][n];
+    }
+}
