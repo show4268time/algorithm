0d2eb598702e592a53faca2d6d673e037d8125d7
show4268time <show4268time@163.com>
2022-09-10 00:04:40 +0800

    剑指 Offer 03. 数组中重复的数字

--- src/main/java/offer/II/Offer03FindRepeatNumber.java
+++ src/main/java/offer/II/Offer03FindRepeatNumber.java
@@ -4,22 +4,27 @@ package offer.II;
  * @author: PhilipFry
  * @create: 2022-08-12 21:18
  * @Description: 找出数组中重复的数字。
- * <p>
- * <p>
  * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：
+ * [2, 3, 1, 0, 2, 5, 3]
+ * 输出：2 或 3
  */
 public class Offer03FindRepeatNumber {
     public int findRepeatNumber(int[] nums) {
+        int n = nums.length;
         int i = 0;
-        while (i < nums.length) {
-            if (nums[i] != i) {
-                if (nums[i] == nums[nums[i]]) return nums[i];
-                int temp = nums[i];
-                nums[i] = nums[temp];
-                nums[temp] = temp;
-            } else {
+        while(i < n){
+            if(nums[i] == i){
                 i++;
+                continue;
             }
+            if(nums[i] == nums[nums[i]]) return nums[i];
+            int temp = nums[i];
+            nums[i] =  nums[temp];
+            nums[temp] = temp;
         }
         return -1;
     }
