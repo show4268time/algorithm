d278298bef54dbd97c8e78694b37f471d5290f35
show4268time <show4268time@163.com>
2022-09-10 00:12:05 +0800

    240. Search a 2D Matrix II

--- src/main/java/leetcode/Code240SearchMatrix.java
+++ src/main/java/leetcode/Code240SearchMatrix.java
@@ -3,24 +3,24 @@ package leetcode;
 /**
  * @author: PhilipFry
  * @create: 2022-07-29 22:49
- * @Description: 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
- * <p>
- * 每行的元素从左到右升序排列。
- * <p>
- * 每列的元素从上到下升序排列。
+ * @Description: Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix.
+ *
+ * This matrix has the following properties:
+ *
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
  */
 public class Code240SearchMatrix {
 
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = 0;
-        int n = matrix[0].length - 1;
-        while (m < matrix.length && n >= 0) {
-            if (matrix[m][n] == target) {
-                return true;
-            } else if (matrix[m][n] > target) {
-                n--;
+        int n = 0, m = matrix[0].length - 1;
+        while (n < matrix.length && m >= 0) {
+            if (matrix[n][m] > target) {
+                m--;
+            } else if (matrix[n][m] < target) {
+                n++;
             } else {
-                m++;
+                return true;
             }
         }
         return false;
