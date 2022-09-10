6103f919cfe82d952ad435081b5b59e869f12017
show4268time <show4268time@163.com>
2022-09-11 00:01:28 +0800

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- src/main/java/leetcode/code200/Code105BuildTree.java
+++ src/main/java/leetcode/code200/Code105BuildTree.java
@@ -6,8 +6,7 @@ import java.util.Map;
 /**
  * @author: PhilipFry
  * @create: 2022-05-14 12:01
- * @Description:
- * Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and
+ * @Description: Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and
  * inorder is the inorder traversal of the same tree,
  * construct and return the binary tree.
  */
@@ -22,26 +21,25 @@ public class Code105BuildTree {
         }
     }
 
-    private Map<Integer, Integer> map = new HashMap<>();
+    Map<Integer, Integer> map = new HashMap<>();
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         for (int i = 0; i < inorder.length; i++) {
             map.put(inorder[i], i);
         }
-        return build(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+        return dfs(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
     }
 
-    public TreeNode build(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd) {
-        if (preStart > preEnd) return null;
-        int val = preorder[preStart];
-        int mid = map.get(val);
-        TreeNode root = new TreeNode(val);
+    private TreeNode dfs(int[] preorder, int[] inorder, int lStart, int lEnd, int rStart, int rEnd) {
+        if (lStart > lEnd) return null;
 
-        int leftSize = mid - inStart;
+        TreeNode node = new TreeNode(preorder[lStart]);
+        int n = map.get(preorder[lStart]);
+        int r = rEnd - n;
+        int l = n - rStart;
+        node.left = dfs(preorder, inorder, lStart + 1, lStart + l, rStart, n - 1);
+        node.right = dfs(preorder, inorder, lStart + l + 1, lEnd, n + 1, n + r);
+        return node;
 
-        root.left = build(preorder, inorder, preStart + 1, preStart + leftSize, inStart, mid - 1);
-        root.right = build(preorder, inorder, preStart + leftSize + 1, preEnd, mid + 1, inEnd);
-
-        return root;
     }
 }
