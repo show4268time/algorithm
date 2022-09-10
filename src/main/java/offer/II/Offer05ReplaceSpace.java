0e52b60ffee0ca558d3b870a123effc2f373c6f6
show4268time <show4268time@163.com>
2022-09-10 23:07:20 +0800

    剑指 Offer 05. 替换空格

--- src/main/java/offer/II/Offer05ReplaceSpace.java
+++ src/main/java/offer/II/Offer05ReplaceSpace.java
@@ -7,16 +7,15 @@ package offer.II;
  */
 public class Offer05ReplaceSpace {
     public String replaceSpace(String s) {
-        char[] chars = s.toCharArray();
-        StringBuilder res = new StringBuilder();
-        for(int i = 0; i < chars.length; i++){
-            if(chars[i] == ' '){
-                res.append("%20");
-            } else{
-                res.append(chars[i]);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == ' '){
+                sb.append("%20");
+            }else{
+                sb.append(c);
             }
-
         }
-        return res.toString();
+        return sb.toString();
     }
 }
