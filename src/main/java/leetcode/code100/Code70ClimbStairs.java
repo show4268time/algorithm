1f87ba7d9b7468e5796b1cf75437f6e7160a8459
show4268time <show4268time@163.com>
2022-09-11 16:34:30 +0800

    1038. Binary Search Tree to Greater Sum Tree

--- src/main/java/leetcode/code100/Code70ClimbStairs.java
+++ src/main/java/leetcode/code100/Code70ClimbStairs.java
@@ -9,13 +9,16 @@ package leetcode.code100;
  */
 public class Code70ClimbStairs {
     public int climbStairs(int n) {
-        if (n == 1) return 1;
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        int m = 1000000007;
+        int a = 1, b = 1;
+        int res = 0;
+        if(n == 0) return a;
+        if(n == 1) return b;
+        for(int i = 2; i <= n; i++){
+            res = (a + b) % m;
+            a = b;
+            b = res;
         }
-        return dp[n];
+        return res;
     }
 }
