85004bdb9c013fdfaf033fc1ad423a528afe5397
show4268time <show4268time@163.com>
2022-09-18 16:50:33 +0800

    53. Maximum Subarray

--- src/main/java/leetcode/code100/Code53MaxSubArray.java
+++ src/main/java/leetcode/code100/Code53MaxSubArray.java
@@ -3,24 +3,33 @@ package leetcode.code100;
 /**
  * @author: PhilipFry
  * @create: 2022-06-14 23:34
- * @Description: Given an integer array nums, find the contiguous (邻接) subarray (containing at least one number) which has the largest sum and return its sum.
+ * @Description: Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
  * <p>
- * A subarray is a contiguous (邻接) part of an array.
+ * A subarray is a contiguous part of an array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * Example 2:
+ * <p>
+ * Input: nums = [1]
+ * Output: 1
+ * Example 3:
+ * <p>
+ * Input: nums = [5,4,-1,7,8]
+ * Output: 23
  */
 public class Code53MaxSubArray {
     public int maxSubArray(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
-        }
-
         int res = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            res = Math.max(res, dp[i]);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum < 0) sum = nums[i];
+            else sum += nums[i];
+            res = Math.max(res, sum);
         }
-
         return res;
     }
 }
