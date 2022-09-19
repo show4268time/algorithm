37b70e3522089e268ce92ed18fbfb770abe8a21f
show4268time <show4268time@163.com>
2022-09-19 23:45:03 +0800

    剑指 Offer 53 - II. 0～n-1中缺失的数字

---
+++ src/main/java/offer/II/Offer53MissingNumber.java
@@ -0,0 +1,30 @@
+package offer.II;
+
+/**
+ * @author: PhilipFry
+ * @create: 2022-09-19 23:44
+ * @Description: 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [0,1,3]
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: [0,1,2,3,4,5,6,7,9]
+ * 输出: 8
+ *  
+ */
+public class Offer53MissingNumber {
+    public int missingNumber(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] > mid) r = mid;
+            else l = mid + 1;
+        }
+        return nums[r] != r ? r : nums[r] + 1;
+    }
+}
