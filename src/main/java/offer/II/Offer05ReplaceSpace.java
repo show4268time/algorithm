fa9cde1ef74f597ac7ae323a24ff5eb501d03a04
show4268time <show4268time@163.com>
2022-09-24 23:11:02 +0800

    剑指 Offer 05. 替换空格

--- src/main/java/offer/II/Offer05ReplaceSpace.java
+++ src/main/java/offer/II/Offer05ReplaceSpace.java
@@ -4,15 +4,23 @@ package offer.II;
  * @author: PhilipFry
  * @create: 2022-08-09 23:30
  * @Description: 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ *  
  */
 public class Offer05ReplaceSpace {
     public String replaceSpace(String s) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if(c == ' '){
+            if (c == ' ') {
                 sb.append("%20");
-            }else{
+            } else {
                 sb.append(c);
             }
         }
