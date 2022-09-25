a1bd7a72ef03307e875dff88fcd7e8e86071a5ff
show4268time <show4268time@163.com>
2022-09-25 17:52:53 +0800

    67. Add Binary

--- src/main/java/leetcode/code100/Code67AddBinary.java
+++ src/main/java/leetcode/code100/Code67AddBinary.java
@@ -1,7 +1,5 @@
 package leetcode.code100;
 
-import java.util.Stack;
-
 /**
  * @author: PhilipFry
  * @create: 2022-08-13 15:05
@@ -18,29 +16,21 @@ import java.util.Stack;
  */
 public class Code67AddBinary {
     public String addBinary(String a, String b) {
-        char[] ac = a.toCharArray();
-        char[] bc = b.toCharArray();
+        char[] ca = a.toCharArray();
+        char[] cb = b.toCharArray();
+
+        int la = ca.length;
+        int lb = cb.length;
+
+        int mod = 0;
         StringBuilder sb = new StringBuilder();
-        Stack<Character> aStack = new Stack<>();
-        Stack<Character> bStack = new Stack<>();
-
-        for (int i = 0; i < ac.length; i++) {
-            aStack.push(ac[i]);
-        }
-
-        for (int i = 0; i < bc.length; i++) {
-            bStack.push(bc[i]);
-        }
-        int pre = 0;
-        while (aStack.size() > 0 || bStack.size() > 0 || pre > 0) {
-            int n = 0;
-            if (aStack.size() > 0) n += aStack.pop() - '0';
-            if (bStack.size() > 0) n += bStack.pop() - '0';
-            n += pre;
+        while (la > 0 || lb > 0 || mod > 0) {
+            int n = (la > 0 ? ca[la - 1] - '0' : 0) + (lb > 0 ? cb[lb - 1] - '0' : 0) + mod;
             sb.insert(0, n % 2);
-            pre = n / 2;
+            mod = n / 2;
+            la--;
+            lb--;
         }
-
         return sb.toString();
     }
 }
