0229e7b04b6f9f472f03ee4be5588ade4c6a09ec
show4268time <show4268time@163.com>
2022-10-11 22:43:23 +0800

    199. Binary Tree Right Side View

--- src/main/java/leetcode/code200/Code199RightSideView.java
+++ src/main/java/leetcode/code200/Code199RightSideView.java
@@ -10,23 +10,22 @@ import java.util.Queue;
  * @create: 2022-05-21 09:43
  * @Description: Given the root of a binary tree, imagine yourself standing on the right side of it,
  * return the values of the nodes you can see ordered from top to bottom.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example 1:
- *
- *
+ * <p>
+ * <p>
  * Input: root = [1,2,3,null,5,null,4]
  * Output: [1,3,4]
  * Example 2:
- *
+ * <p>
  * Input: root = [1,null,3]
  * Output: [1,3]
  * Example 3:
- *
+ * <p>
  * Input: root = []
  * Output: []
- *
  */
 public class Code199RightSideView {
     public class TreeNode {
@@ -51,22 +50,19 @@ public class Code199RightSideView {
     private List<Integer> result = new ArrayList<>();
 
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if (root == null) return result;
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
         while (q.size() > 0) {
-            int l = q.size();
-            int n = 0;
-            for (int i = 0; i < l; i++) {
+            int n = q.size();
+            for (int i = n; i > 0; i--) {
                 TreeNode node = q.poll();
-
-                n = node.val;
-                if (node.left != null) q.offer(node.left);
+                if (i == n) res.add(node.val);
                 if (node.right != null) q.offer(node.right);
+                if (node.left != null) q.offer(node.left);
             }
-            result.add(n);
         }
-        return result;
+        return res;
     }
 }
