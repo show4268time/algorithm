5d15322fa7f66f6db0c347033d5130a67d435b1d
show4268time <show4268time@163.com>
2022-10-12 21:56:25 +0800

    129. Sum Root to Leaf Numbers

--- src/main/java/leetcode/code200/Code129SumNumbers.java
+++ src/main/java/leetcode/code200/Code129SumNumbers.java
@@ -4,11 +4,33 @@ package leetcode.code200;
  * @author: PhilipFry
  * @create: 2022-05-20 18:07
  * @Description: You are given the root of a binary tree containing digits from 0 to 9 only.
+ * <p>
  * Each root-to-leaf path in the tree represents a number.
+ * <p>
  * For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
- * Return the total sum of all root-to-leaf numbers.
- * Test cases are generated so that the answer will fit in a 32-bit integer.
+ * Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+ * <p>
  * A leaf node is a node with no children.
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: root = [1,2,3]
+ * Output: 25
+ * Explanation:
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Therefore, sum = 12 + 13 = 25.
+ * Example 2:
+ * <p>
+ * <p>
+ * Input: root = [4,9,0,5,1]
+ * Output: 1026
+ * Explanation:
+ * The root-to-leaf path 4->9->5 represents the number 495.
+ * The root-to-leaf path 4->9->1 represents the number 491.
+ * The root-to-leaf path 4->0 represents the number 40.
+ * Therefore, sum = 495 + 491 + 40 = 1026.
  */
 public class Code129SumNumbers {
 
@@ -43,5 +65,25 @@ public class Code129SumNumbers {
         if (node.left == null && node.right == null) return pre;
 
         return dfs(node.left, pre) + dfs(node.right, pre);
+    }
+
+    int res = 0;
+
+    public int sumNumbers1(TreeNode root) {
+        if (root == null) return res;
+        dfs1(root, 0);
+        return res;
+    }
+
+    private void dfs1(TreeNode node, int pre) {
+
+        pre = pre * 10 + node.val;
+        if (node.left == null && node.right == null) {
+            res += pre;
+            return;
+        } else {
+            if (node.left != null) dfs1(node.left, pre);
+            if (node.right != null) dfs1(node.right, pre);
+        }
     }
 }
