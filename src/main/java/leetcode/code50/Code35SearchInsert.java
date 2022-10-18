7c9eca98428234d66531a9ed8e20f99ad6258552
show4268time <show4268time@163.com>
2022-10-18 23:34:33 +0800

    35. Search Insert Position

--- src/main/java/leetcode/code50/Code35SearchInsert.java
+++ src/main/java/leetcode/code50/Code35SearchInsert.java
@@ -18,10 +18,10 @@ package leetcode.code50;
 public class Code35SearchInsert {
     public int searchInsert(int[] nums, int target) {
         int l = 0, r = nums.length - 1;
-        if(nums[0] >= target) return 0;
-        while(l < r){
+        if (nums[0] >= target) return 0;
+        while (l < r) {
             int mid = l + r + 1 >> 1;
-            if(nums[mid] < target) l = mid;
+            if (nums[mid] < target) l = mid;
             else r = mid - 1;
         }
         return l + 1;
