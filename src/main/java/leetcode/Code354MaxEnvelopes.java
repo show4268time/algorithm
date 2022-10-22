79877c147cc09c68fb3b50ddda4f5de8c8000bb0
show4268time <show4268time@163.com>
2022-10-22 19:39:37 +0800

    875. Koko Eating Bananas

--- src/main/java/leetcode/Code354MaxEnvelopes.java
+++ src/main/java/leetcode/Code354MaxEnvelopes.java
@@ -15,12 +15,8 @@ public class Code354MaxEnvelopes {
     public int maxEnvelopes(int[][] envelopes) {
         int n = envelopes.length;
         // 按宽度升序排列，如果宽度一样，则按高度降序排列
-        Arrays.sort(envelopes, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[0] == b[0] ?
-                        b[1] - a[1] : a[0] - b[0];
-            }
-        });
+        Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ?
+                b[1] - a[1] : a[0] - b[0]);
         // 对高度数组寻找 LIS
         int[] height = new int[n];
         for (int i = 0; i < n; i++)
