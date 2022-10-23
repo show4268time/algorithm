11e8bdb5479d445cdc5ebdca251489d4ff02671e
show4268time <show4268time@163.com>
2022-10-23 23:18:27 +0800

    47. Permutations II

--- src/main/java/leetcode/code50/Code47PermuteUnique.java
+++ src/main/java/leetcode/code50/Code47PermuteUnique.java
@@ -14,31 +14,30 @@ import java.util.List;
 public class Code47PermuteUnique {
     List<List<Integer>> res = new ArrayList<>();
     LinkedList<Integer> path = new LinkedList<>();
+
     boolean[] used;
+
     public List<List<Integer>> permuteUnique(int[] nums) {
-        used = new boolean[nums.length];
         Arrays.sort(nums);
+        used = new boolean[nums.length];
         dfs(nums);
         return res;
     }
 
-    private void dfs(int[] nums){
-        if(path.size() == nums.length){
+    private void dfs(int[] nums) {
+        if (path.size() == nums.length) {
             res.add(new ArrayList<>(path));
             return;
         }
 
-        for(int i = 0; i < nums.length; i++){
-            if(used[i]) continue;
-
-
-            if(i > 0 && nums[i -1] == nums[i] && !used[i -1]) continue;
-
-            used[i]=true;
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            if (i > 0 && !used[i - 1] && nums[i] == nums[i - 1]) continue;
+            used[i] = true;
             path.add(nums[i]);
             dfs(nums);
             path.removeLast();
-            used[i]=false;
+            used[i] = false;
         }
     }
 }
