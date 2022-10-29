da4d8b9fb2b2ebeb6a3e2b18547bc25145861488
show4268time <show4268time@163.com>
2022-10-29 18:50:11 +0800

    1143. Longest Common Subsequence

--- src/main/java/leetcode/Code1143LongestCommonSubsequence.java
+++ src/main/java/leetcode/Code1143LongestCommonSubsequence.java
@@ -4,11 +4,28 @@ package leetcode;
  * @author: PhilipFry
  * @create: 2022-06-16 23:14
  * @Description: Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
- * <p>
+ *
  * A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
- * <p>
+ *
  * For example, "ace" is a subsequence of "abcde".
  * A common subsequence of two strings is a subsequence that is common to both strings.
+ *
+ * Example 1:
+ *
+ * Input: text1 = "abcde", text2 = "ace"
+ * Output: 3
+ * Explanation: The longest common subsequence is "ace" and its length is 3.
+ * Example 2:
+ *
+ * Input: text1 = "abc", text2 = "abc"
+ * Output: 3
+ * Explanation: The longest common subsequence is "abc" and its length is 3.
+ * Example 3:
+ *
+ * Input: text1 = "abc", text2 = "def"
+ * Output: 0
+ * Explanation: There is no such common subsequence, so the result is 0.
+ *
  */
 public class Code1143LongestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
