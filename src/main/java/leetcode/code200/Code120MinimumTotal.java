c21972939a7f9ec8a4634906aece30bf8be5d9c0
show4268time <show4268time@163.com>
2022-10-30 10:11:38 +0800

    120. Triangle

--- src/main/java/leetcode/code200/Code120MinimumTotal.java
+++ src/main/java/leetcode/code200/Code120MinimumTotal.java
@@ -5,10 +5,24 @@ import java.util.List;
 /**
  * @author: PhilipFry
  * @create: 2022-06-14 23:06
- * @Description: Given a triangle array, return the minimum (最低) path sum from top to bottom.
+ * @Description: Given a triangle array, return the minimum path sum from top to bottom.
  * <p>
- * For each step, you may move to an adjacent (相邻) number of the row below. More formally (正式) ,
- * if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+ * For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+ * Output: 11
+ * Explanation: The triangle looks like:
+ * 2
+ * 3 4
+ * 6 5 7
+ * 4 1 8 3
+ * The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+ * Example 2:
+ * <p>
+ * Input: triangle = [[-10]]
+ * Output: -10
  */
 public class Code120MinimumTotal {
     public int minimumTotal(List<List<Integer>> triangle) {
