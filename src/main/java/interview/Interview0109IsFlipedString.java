a69edd6a6a400de574d0a81aca514445f0597f25
show4268time <show4268time@163.com>
2022-11-09 21:38:43 +0800

    面试题 01.09. 字符串轮转

--- src/main/java/interview/Interview0109IsFlipedString.java
+++ src/main/java/interview/Interview0109IsFlipedString.java
@@ -20,4 +20,23 @@ public class Interview0109IsFlipedString {
 
         return (s2 + s2).contains(s1);
     }
+
+    public boolean isFlipedString1(String s1, String s2) {
+        if(s1.equals("") && s2.equals("")) return true;
+        if(s1.length() != s2.length()) return false;
+        int n = s1.length();
+        s1 = s1 + s1;
+        for(int i = 0; i < n; i++){
+            if(check(s1.substring(i, n + i), s2)) return true;
+        }
+        return false;
+    }
+
+    private boolean check(String s1, String s2){
+        int n = s1.length();
+        for(int i = 0; i < n; i++){
+            if(s1.charAt(i) != s2.charAt(i)) return false;
+        }
+        return true;
+    }
 }
