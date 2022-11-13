0e89eb8e36bcb643200908c4070122fe7418d0a1
show4268time <show4268time@163.com>
2022-11-13 18:27:27 +0800

    面试题 02.07. 链表相交

--- src/main/java/interview/Interview0207GetIntersectionNode.java
+++ src/main/java/interview/Interview0207GetIntersectionNode.java
@@ -38,4 +38,19 @@ public class Interview0207GetIntersectionNode {
 
         return node1;
     }
+
+    public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        ListNode curA = headA;
+        ListNode curB = headB;
+        while(curA != null || curB != null){
+            if(curA == null) curA = headB;
+            if(curB == null) curB = headA;
+
+            if(curA == curB && curA != null && curB != null) return curA;
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
 }
